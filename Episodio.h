53e4a96e50eb3cf55453d7ad664d4495193c4ae5
greziatrujillo <a01713876@tec.mx>
2025-06-02 12:53:59 -0600

    Create Episodio.h

---
+++ Episodio.h
@@ -0,0 +1,78 @@
+/*
+ *
+ * Proyecto Tipo de video
+ * clase Episodio para poder definir episodios y sus atributos
+ * importante tener antes de clase serie para que clase serie pueda usar la informacion inicializada aqui
+ * Grezia Trujillo
+ * A01713876
+ * 05/06/2025
+ * 
+ */
+
+#ifndef EPISODIO_H
+#define EPISODIO_H
+
+#include <iostream>
+#include "Video.h"
+using namespace std;
+
+class Episodio {
+     private:
+         int temporada;
+         string titulo;
+         float calificacion;
+         int episodeNumber;
+
+     public:
+     //constructor
+        Episodio(string title, int temp, int epNum, float calif) {
+            titulo = title;
+            temporada = temp;
+            episodeNumber = epNum;
+            calificacion = calif;
+        }
+
+        //getters
+        string getTitulo() {
+            return titulo;
+        }   
+
+        int getTemporada() {
+            return temporada;
+        }
+
+        int getEpisodeNumber() {
+            return episodeNumber;
+        }
+
+        float getCalificacion() {
+            return calificacion;
+        }
+
+        //setters
+        void setCalificacion(float calif) {
+            calificacion = calif;
+        } 
+
+        void setTemporada(int temp) {
+            temporada = temp;
+        }
+
+        void setTitulo(string title) {
+            titulo = title;
+        }
+
+        void setEpisodeNumber(int epiNum) {
+            episodeNumber = epiNum;
+        }
+
+        //mostramos infomacion al usuario de episodios ya que pide titulo, numero de episodio y temporada de cada uno
+        void mostrarInfo() {
+            cout << "Nombre episodio: " << getTitulo()
+                 << " |Temporada: " << getTemporada()
+                 << " |Episodio: " << getEpisodeNumber()
+                 << " |Calificacion: " << getCalificacion() << endl<< endl;
+        }
+ };
+
+#endif
